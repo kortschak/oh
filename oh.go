de3b54c1a40312f0c4186dd9de7a7e6782db39ee
Dan Kortschak <dan@kortschak.io>
2025-11-23 11:36:05 +1030

    fix grammar

--- oh.go
+++ oh.go
@@ -4,9 +4,9 @@ package oh
 import _ "unsafe"
 
 // The name of the function provided by this package was intentionally
-// chosen to be offensive to the reduce the likelihood that someone
-// would get its use through code review. It is not intended to be used
-// in production code.
+// chosen to be offensive to reduce the likelihood that someone would
+// get its use through code review. It is not intended to be used in
+// production code.
 
 // Fuck is a linkname alias to runtime.throw.
 //
